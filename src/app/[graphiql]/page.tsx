434519b96bd8c4141b09aeabe54b6fbbc1801d3f
seenpie <bananananasez@gmail.com>
2024-09-13 21:40:52 +0300

    feat: implement GraphQl page and minor changes in globals css and layout

---
+++ src/app/[graphiql]/page.tsx
@@ -0,0 +1,5 @@
+import { GraphiQl } from "@/views/GraphiQl/GraphiQl";
+
+export default function GraphQlPage() {
+  return <GraphiQl />;
+}
--- src/app/graphql/page.tsx
+++
@@ -1,5 +0,0 @@
-import { GraphQlPlayground } from "@/components/playgrounds/GraphQlPlayground";
-
-export default function GraphQlPage() {
-  return <GraphQlPlayground />;
-}
